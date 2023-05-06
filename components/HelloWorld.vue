0b9fe443c456a5e56a621ac8a97d381a2d52caeb
Michael Harris <michael.harris.sweng@gmail.com>
2023-05-05 18:38:45 -0700

    MHIO-1: Setup project

---
+++ components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="hello-world">
+        <h1>Hello World!</h1>
+        <h3 v-if="visible" @click="visible = false">Click Me!</h3>
+    </div>
+</template>
+
+<script lang="ts">
+export default defineNuxtComponent({
+    name: "Hello World",
+    setup(props, context) {
+        const visible: Ref<boolean> = ref(true);
+
+        return {
+            visible,
+        }
+    },
+})
+</script>
+
+<style lang="scss">
+.hello-world {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+    h3 {
+        cursor: pointer;
+    }
+}
+</style>
